8f4a89e4aeba8c8a2083966f0e4c7f09c863cfe7
snadler <sharonn@dropbox.com>
2018-02-15 11:14:48 +0200

    Adding pycharm folder calc (now independent of pycharm version

--- open_files.py
+++ open_files.py
@@ -3,15 +3,23 @@
 import string
 import xml.etree.ElementTree as ET
 import os
+import commands
 
 PROJECT_RELATIVE_PATH = '/src/server/.idea/workspace.xml'
 HOME_PATH = os.path.expanduser("~")
 WORKSPACE_FILE_PATH =  HOME_PATH + PROJECT_RELATIVE_PATH
 
+
+def get_pycharm_folder():
+    status, output = commands.getstatusoutput("find  ~//Library/Preferences -type d | grep -m1 PyCharm")
+    return output.split('/')[-1]
+
+
 # Pretty stupid function. TODO- check if there is other way for doing this, and if we need python 3.2 for "elem.iter"
 def get_sub_elem(elem, sub_elem_name):
     for sub_elem in elem.iter(sub_elem_name):
         return sub_elem
+
 
 def get_file_editor_component(root):
     components = root.iterfind('component')
@@ -19,7 +27,9 @@ def get_file_editor_component(root):
         if component.attrib['name'] == 'FileEditorManager':
             return component
 
+
 def get_open_files_paths(files_editor_manager):
+    pycharm_folder = get_pycharm_folder()
     open_files_raw_paths = []
     active_file_raw_path = None
     for file in files_editor_manager.iter('file'):
@@ -34,15 +44,17 @@ def get_open_files_paths(files_editor_manager):
     open_files_info = []
     for file_raw_path, line in open_files_raw_paths:
         file_path = string.replace(file_raw_path, 'file://$PROJECT_DIR$', '~/src/server')
-        file_path = string.replace(file_path, 'file://$APPLICATION_CONFIG_DIR$', '~/Library/Preferences/PyCharm2017.3') # TODO: don't assume pycharm version
+        file_path = string.replace(file_path, 'file://$APPLICATION_CONFIG_DIR$', '~/Library/Preferences/' + pycharm_folder)
         open_files_info.append((file_path, line))
     return open_files_info
+
 
 def open_files(open_files_info):
     command = '/Applications/PyCharm.app/Contents/MacOS/pycharm ~/src/server'
     for file_path, line in open_files_info:
         command += ' --line ' + line + " " + file_path
     os.system(command)
+
 
 def main():
     tree = ET.parse(WORKSPACE_FILE_PATH)
